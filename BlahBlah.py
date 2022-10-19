d9339b4c1521990c2739b3ada58dac6f62634aeb
Victor LaBrie <vplabrie@gmail.com>
2022-10-19 12:45:27 -0500

    Added code from lab2.

--- BlahBlah.py
+++ BlahBlah.py
@@ -14,13 +14,10 @@ def main():
     #First, we need to set this up to get the command line arguments.
     #n and m can be grabbed from the first two arguments
     n = sys.argv[1]
-    #print(n, type(n))
     m = sys.argv[2]
-    #print(m, type(m))
     #Since there can be multiple text files, we can just use list slicing to
     #get the list of text files:
     textFiles = sys.argv[3:]
-    #print(textFiles)
     #I will then validate the input:
     if not re.match(r'^[1-9]+[0-9]*$', n):
         raise ValueError("First argument must be a valid integer.")
@@ -31,16 +28,48 @@ def main():
             raise ValueError("Invalid text file. Text files must end with \".txt\".")
     #Then, convert all text to lower case and store in a string:
     print(n,m, *textFiles)
+    n = int(n)
+    m = int(m)
     allText = ""
     for txt in textFiles:
         thisFile = open(txt)
         thisText = thisFile.read().lower()
         allText += thisText
         thisFile.close()
-    #print(len(allText))
-    #print("No Errors.")
+    #Remove all non alpha-text:
+    cleanedText = re.sub(r"[^a-zA-Z_ \.\,\?\!]", "", allText)
+    #Put space between words and punctuation
+    cleanedText = re.sub(r"([,\.\?\!])", r" \1 ", cleanedText)
+    #Call ngram model generator and printer:
+    ngrams(n, cleanedText)
+    
+    #Last line printed will be number of tokens in corpus.
+    print("Tokens in corpus:", len(cleanedText))
     return 0
 
-
+#Function for generating ngram model
+def ngrams(n, text):
+    wordDict = {}
+    tokenSum = 0
+    #Look at each word
+    for i in text.split():
+            tokenSum += 1
+            if i not in wordDict:
+                wordDict[i] = 1
+            else:
+                wordDict[i] += 1
+            
+    #next, we sort the dictionary
+    sortedWordList = sorted(wordDict, key = wordDict.get, reverse=True)
+    sortedWordDict = {}
+    for y in sortedWordList:
+        sortedWordDict[y]=wordDict[y]
+    sortedKeys = list(sortedWordDict.keys())
+    sortedItems = list(sortedWordDict.items())
+    print("Number of tokens: ", tokenSum)
+    print("Number of types: ", len(sortedKeys))
+    for i in range(0,n):
+        thisItem = sortedItems[i]
+        print(thisItem)
 
 main()
